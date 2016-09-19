a0f73e830dea6b17001fccb219149762a55f0320
Justin Shrake <justinshrake@gmail.com>
2016-09-18 17:54:31 -0700

    Add some sanity tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using Hearthstone
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@test_approx_eq_eps Hearthstone.card_in_opening_hand_coin(2) 0.5098 1e-4
+@test_approx_eq_eps Hearthstone.card_in_opening_hand_no_coin(2) 0.4125 1e-4
+@test_approx_eq Hearthstone.card_in_next_N_turns(1, 20, 1) 1/20
+@test Hearthstone.missile_lethal(10, 8, 2) == 1.0
